53b1c069dc44d8554426b5d887e721a8114dc2cd
Diana Pesut <Diana.Pesut@uni-duesseldorf.de>
2013-05-29 10:39:49 +0200

    new changes

--- src/gruppe63_dungeon_crawler/Controller.java
+++ src/gruppe63_dungeon_crawler/Controller.java
@@ -1,33 +1,61 @@
-package gruppe63_dungeon_crawler;
+package gruppe63_dungeon_crawler;	
 
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JLabel;
-
-public abstract class Controller extends JFrame{
+public class Controller extends KeyAdapter{	
+	private Player player;
 	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public Controller(String text)
-	{
-		super(text);
+	public Controller(Player player){
+	this.player = player;
+	}
+	public Controller(){
+	}
+	public void setPlayer(Player player){
+	this.player=player;
 	}
 	
-	public abstract void initialize();
+	public void keyPressed(KeyEvent e){	
+		int key = e.getKeyCode();
+	
+	    if (key == KeyEvent.VK_LEFT) {		// Wenn Pfeiltaste gedrückt
+	        player.setMovX(-1);				// dann stelle äquivalente Bewegungsrichtung ein
+	    }
+	
+	    if (key == KeyEvent.VK_RIGHT) {
+	    	player.setMovX(1);
+	    }
+	
+	    if (key == KeyEvent.VK_UP) {
+	        player.setMovY(1);
+	    }
+	    
+	    if (key == KeyEvent.VK_DOWN) {
+	        player.setMovY(-1);
+	    }
+	}
+	public void keyReleased(KeyEvent e){
+		int key = e.getKeyCode();
+	
+	    if (key == KeyEvent.VK_LEFT) {			// Wenn Taste losgelassen
+	        if(player.getMovX()==-1)
+	        	player.setMovX(0);					// dann setze Bewegungsbariablen zurück
+	    }
+	
+	    if (key == KeyEvent.VK_RIGHT) {
+	    	if(player.getMovX()==1)
+	    		player.setMovX(0);	
+	    }
+	
+	    if (key == KeyEvent.VK_UP) {
+	    	if(player.getMovY()==1)
+	    		player.setMovY(0);	
+	    }
+	
+	    if (key == KeyEvent.VK_DOWN) {
+	    	if(player.getMovY()==-1)
+	    		player.setMovY(0);	
+	    }
+	}
 
 }
---
+++ src/gruppe63_dungeon_crawler/Player.java
@@ -0,0 +1,59 @@
+package gruppe63_dungeon_crawler; 
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
+
+
+@SuppressWarnings({ "serial" })
+public class Player extends Elements {
+	private Room room;
+		
+	public Player(int x, int y, Room room){
+		//Groesse des Spielers
+		super(x,y,30,30);
+		this.room = room;
+	}
+	
+	public void move(){
+		x=this.getX();
+		y=this.getY();
+			
+		if(room.Environment(x+vx,y,xDim,yDim)!=1){	
+			x = x+vx;
+			this.setLocation(x,y);
+		}
+		
+		if(room.Environment(x,y-vy,xDim,yDim)!=1){	
+			y = y-vy;
+			this.setLocation(x,y);
+		}
+		room.status();
+	}
+	
+	public void paintComponent(Graphics g){
+	Image img = Toolkit.getDefaultToolkit().getImage("ghost.gif");
+	g.drawImage(img, 0, 0, this);
+	Toolkit.getDefaultToolkit().sync();
+	g.dispose();
+	}
+	
+	public void setMovX(int vx) {
+		this.vx = vx;
+	}
+	
+	public void setMovY(int vy) {
+		this.vy = vy;
+	}
+	
+	public int getMovX() {
+		return vx;
+	}
+	
+	public int getMovY() {
+		return vy;
+	}
+	
+
+}
+
